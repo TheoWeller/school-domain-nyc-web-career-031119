9f79d64aa6b01a5842ffd92fc773f126f0b42edc
Theo Weller <weller.theodor@gmail.com>
2019-03-13 20:44:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name
+
+def initialize(name)
+  @name = name
+  def roster
+  @hash = Hash.new
+  end
+end
+
+def add_student(student_name, grade)
+  #@grade = grade
+  @hash[grade] ||= []
+  @hash[grade] << student_name
+end
+
+
+end
+#binding.pry
